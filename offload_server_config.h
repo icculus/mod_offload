530e2eacf2319cb46d98ea37f20915233ac10d1f
Ryan C. Gordon <icculus@icculus.org>
2008-08-31 00:29:19 -0400

    Wrap config macros in #ifndef blocks so we can override from command line.

--- offload_server_config.h
+++ offload_server_config.h
@@ -3,20 +3,30 @@
 
 // GDEBUG should be false at production time, but this lets you sanity check
 //  some things before going live.
+#ifndef GDEBUG
 #define GDEBUG 0
+#endif
 
 // This is a list of servers that you are offloading.
+#ifndef GBASESERVER
 #define GBASESERVER "icculus.org"
+#endif
 
 // Time in seconds that i/o to base server should timeout in lieu of activity.
+#ifndef GTIMEOUT
 #define GTIMEOUT 90
+#endif
 
 // This is where we'll cache files.
+#ifndef GOFFLOADDIR
 #define GOFFLOADDIR "/usr/local/apache/offload"
+#endif
 
 // Set GDEBUGTOFILE to write all debug info to files in GOFFLOADDIR, if
 //  GDEBUG is also true. You probably want this to be true most cases.
+#ifndef GDEBUGTOFILE
 #define GDEBUGTOFILE 1
+#endif
 
 // end of offload_server_config.h ...
 
